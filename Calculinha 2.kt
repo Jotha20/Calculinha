08c85b12d9a1ffec09f1fa91b1fb116d4f9c68c0
Juliano <julianosilveira.dev@gmail.com>
2021-03-01 16:58:35 -0300

    Calculadora simples

--- Calculinha 2.kt
+++ Calculinha 2.kt
@@ -1,17 +1,24 @@
-fun main(){
+
+fun main() {
+
 
     print("Digite um Número Inteiro: ")
-    var a:Double = readLine()!!.toDouble()
+    var a: Double = readLine()!!.toDouble()
+
     print("Digite um Número Inteiro: ")
-    var b:Double = readLine()!!.toDouble()
+    var b: Double = readLine()!!.toDouble()
     println()
 
-    var s:Double = a + b
-    var m:Double = a - b
-    var v:Double = a * b
-    var d:Double = a / b
+
+    var s: Double = a + b
+    var m: Double = a - b
+    var v: Double = a * b
+    var d: Double = a / b
 
     println("O resultado de $a + $b é: $s ")
     println("O resultado de $a - $b é: $m ")
     println("O resultado de $a X $b é: $v ")
     println("O resultado de $a / $b é: $d ")
+
+
+}
